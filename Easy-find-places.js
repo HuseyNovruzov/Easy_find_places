70a965ab0413ea2fd8284afed1d8aee696669f84
Huseyn Novruzov <huseyn.novruzov.123@gmail.com>
2020-08-23 10:35:17 +0300

    body part and javascript was added

--- Easy-find-places.js
+++ Easy-find-places.js
@@ -1,30 +1,63 @@
+//background image slider
+let bg_img,i;
+bg_img = document.querySelector('.bg-img');
+i =0;
+	setInterval(function slide(){
+		let images; 
+		images = [
+		'images/football.jpeg','images/tennis.jpeg',
+		'images/volleyball.jpeg','images/skateboard.jpeg',
+		'images/cycling.jpeg','images/basketball.jpeg'];
+		bg_img.setAttribute('style','background-image: url('+
+			images[i++]+');transition-duration: 3s;');
+		if(i>=images.length){
+			i = 0;
+		}},7000)
+
 let tasker ={
 		construct: function(){
 			this.variables();
 			this.bindEvents();
+			this.scanList();
 		},
 		variables: function(){
 			this.open = true;
-			this.click =  document.querySelector('#do');
-			this.undo = document.querySelector('#undo');
+			this.btns = document.querySelector('.menu-icon');
+			this.btnChilds = this.btns.children;
 			this.navbar = document.querySelector('.navbar');
+			this.underList = document.querySelector('#pagination');
+			this.childList = this.underList.children;
 		},
+		//open and close mobile menu
 		openClose: function(){
 			if(this.open){
 				this.navbar.setAttribute('style','left:0');
-				this.click.setAttribute('style','display:none');
-				this.undo.setAttribute('style','display:block');
+				this.btnChilds[0].setAttribute('style','display:none');
+				this.btnChilds[1].setAttribute('style','display:block');
 				this.open = false;
 			}
 			else{
 				this.navbar.setAttribute('style','left: -100%');
-				this.undo.setAttribute('style','display:none');
-				this.click.setAttribute('style','display:block');
+				this.btnChilds[1].setAttribute('style','display:none');
+				this.btnChilds[0].setAttribute('style','display:block');
 				this.open = true;
 			}
 		},
 		bindEvents: function(){
-			this.click.onclick = this.openClose.bind(this);
-			this.undo.onclick = this.openClose.bind(this);
+			this.btnChilds[0].onclick = this.openClose.bind(this);
+			this.btnChilds[1].onclick = this.openClose.bind(this);
+		},
+		// show current active link
+		scanList: function(){
+		let active,current;
+		for(let i = 0;i<this.childList.length;i++){
+			childList = this.childList[i];
+			current = this.underList.getElementsByClassName("active");
+			childList.onclick = this.activate.bind(this,childList,current);
 		}
+	},
+		activate: function(childList,current){
+			current[0].className = current[0].className.replace("active","");
+			childList.className+="active";
+	}
 	}
